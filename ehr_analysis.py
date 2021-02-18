e3c7c5fee9ce42bfdeb9bef12f55a210a474cbb9
Noel Allen <noelallen@Noels-MBP.localdomain>
2021-02-18 18:44:59 -0500

    age_admin

--- ehr_analysis.py
+++ ehr_analysis.py
@@ -57,29 +57,23 @@ def sick_patients(lab_data, lab, gt_lt, value):
     return matchinglabid
 
 
-<<<<<<< HEAD
-labdata = load_labs("labcorepopulatedtest.txt")
-#print(lab_data_ex['2006-10-13 16:27:58.243'])
+#searchval = search(labcore_dict, '1A8791E3-A61C-455A-8DEE-763EB90C9B2C')
+#print( searchval)
+
 #print(sick_patients(labdata, 20, ">", 4.0 ))
 
 # Returns a list of patient ages.
-
-
 def load_patients(filename):
     """ This Function parses the PatientCorePopulatedTable and converts
     the DOB timestamps into age in years
     There are 4 for loops in this function which equates to o(4n), in big
     O notation, the constant cancels out leaving
     o(n) as the computational complexity."""
-=======
-# Returns a list of patient ages. 
-def load_patients (filename):
->>>>>>> 6766e2ca858e1d207ad56b4afa2054c901436518
     from datetime import datetime
     #from datetime import date
 
     listofages = []
-    with open(filename) as stream:
+    with open(filename, encoding='utf-8-sig') as stream:
         patient_data_string = stream.read()
 
         patient_data_list = patient_data_string.split("\n")
@@ -115,11 +109,21 @@ def load_patients (filename):
         ageinseconds = time_difference.total_seconds()
         age = ageinseconds / 31536000
         listofages.append(age)
+    # remove patientid label
+    if 'PatientID' in dictionary_patient_values:
+        dictionary_patient_values.pop('PatientID')
+    keys = dictionary_patient_values.keys()
+    values = listofages
+    patientagesdictionary = dict(zip(keys, values))
 
-    return listofages
+    return patientagesdictionary
 
 
-def num_older_than(patientages, age):
+# print(load_patients("patientcorepopulatedtest.txt")
+print(load_patients('patientcorepopulatedtest.txt'))
+
+
+def num_older_than(patientagesdictionary, age):
     """This function loops through the data once to determine
 the number of values greater than the age input
  therfore the complexity is O(n) """
@@ -130,21 +134,54 @@ the number of values greater than the age input
         raise ValueError('age should be an int or float')
 
     oldagecounter = 0
-    for patient_age in patientages:
+    for key in patientagesdictionary:
+        patient_age = patientagesdictionary[key]
         if patient_age > age:
             oldagecounter = oldagecounter + 1
     return oldagecounter
-<<<<<<< HEAD
 
 
-patientages = load_patients("patientcorepopulatedtest.txt")
-print(num_older_than(patientages, 70))
+patientagesdictionarytest = load_patients("patientcorepopulatedtest.txt")
+#print(num_older_than(patientagesdictionarytest, 70))
 
 
-def age_admission(patientid, filename):
-    labdata = load_labs(filename)
-=======
-        
-patientages = load_patients("PatientCorePopulatedTable.txt")
-print(num_older_than(patientages, 70))
->>>>>>> 6766e2ca858e1d207ad56b4afa2054c901436518
+def age_admission(patientid, labfile, patientfile):
+    if not isinstance(patientid, str):
+        raise ValueError('patient ID should be a string')
+    from datetime import datetime
+    labdata = load_labs(labfile)
+    patientdata = load_patients(patientfile)
+    #id and ages
+   # print(patientdata)
+    listoftestdates = []
+    patientage = 0
+    #specific_id = [val for key,val in patientdata.items() if search in mystring]
+    if patientid in patientdata:
+        patientage = patientdata.get(patientid)
+
+    patientlabdates = []
+    for key, value in labdata.items():
+        if patientid in value:
+
+            patientlabdates.append(key)
+    list_admin_date_objects = []
+    for i in patientlabdates[1:]:
+        date_admin = datetime.strptime(
+            i, "%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S.%f")
+        list_admin_date_objects.append(date_admin)
+    today = datetime.now()
+
+    oldestlabappointmentdate = min(list_admin_date_objects)
+
+   # patientage/ oldestlabappointmentdate
+    today = datetime.now()
+    oldestlabappointmentdateObject = datetime.strptime(
+        oldestlabappointmentdate, "%Y-%m-%d %H:%M:%S.%f")
+    oldestlabappointmentage = today - oldestlabappointmentdateObject
+    oldestlabappointmentageinyears = oldestlabappointmentage.days/365.25
+    ageatfirstappointment = patientage - oldestlabappointmentageinyears
+
+    return(ageatfirstappointment)
+
+
+#print(age_admission('1A40AF35-C6D4-4D46-B475-A15D84E8A9D5', 'LabsCorePopulatedTable.txt', 'PatientCorePopulatedTable.txt'))
